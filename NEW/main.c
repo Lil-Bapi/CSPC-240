23effd0159a06593e69887e0a29f61e0a24c691c
Lil-Bapi <kquan59@gmail.com>
2024-03-17 20:02:00 -0700

    DONE

--- NEW/main.c
+++ NEW/main.c
@@ -1,3 +1,24 @@
+// Program name: Array Manager a program that takes in the user's inputs of
+// numbers. The inputed values within the array then are displayed and the mean
+// is calculated which is also will be displayed.
+// Copyright (C) <2024>  <Quan Khong>
+
+// "Array Manager" is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// "Array Manager" is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with "Array Manager".  If not, see <https://www.gnu.org/licenses/>.
+
+// Author: Quan Khong
+// Date: 03/17/2024
+
 #include "stdio.h"
 
 extern double manager();
--- NEW/manager.asm
+++ NEW/manager.asm
@@ -1,12 +1,23 @@
-; Author name: Andy Nguyen
-; Author email: andynguyendo@csu.fullerton.edu
-; For: Assignment 2 - Array Management System
-; Purpose of this file:
-; This is the manage.asm module used to create an array of doubles using user input.
-; This assembly module will return an array of doubles, and will modify the parameter
-; coming from rdi to be the number of inputs from the user.
-; Completion Date: 09/23/2023
+; Program name: Array Manager a program that takes in the user's inputs of
+; numbers. The inputed values within the array then are displayed and the mean
+; is calculated which is also will be displayed.
+; Copyright (C) <2024>  <Quan Khong>
 
+; "Array Manager" is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; "Array Manager" is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with "Array Manager".  If not, see <https://www.gnu.org/licenses/>.
+
+; Author: Quan Khong
+; Date: 03/17/2024
 extern printf
 extern scanf
 extern input_array
